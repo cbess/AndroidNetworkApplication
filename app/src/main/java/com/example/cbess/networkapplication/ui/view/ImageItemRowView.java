e24f46d19b375935fad18008998b6083351025eb
C. Bess <cbess@company.com>
2015-11-09 16:10:07 -0600

    - update to use DataBinding for the ListView items

--- app/src/main/java/com/example/cbess/networkapplication/ui/view/ImageItemRowView.java
+++ app/src/main/java/com/example/cbess/networkapplication/ui/view/ImageItemRowView.java
@@ -1,12 +1,14 @@
 package com.example.cbess.networkapplication.ui.view;
 
 import android.content.Context;
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
 import android.view.LayoutInflater;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.example.cbess.networkapplication.R;
+import com.example.cbess.networkapplication.databinding.ViewImageItemRowBinding;
 import com.example.cbess.networkapplication.model.ImageItem;
 import com.squareup.picasso.Picasso;
 
@@ -14,7 +16,14 @@ import com.squareup.picasso.Picasso;
  * Represents the image item row view.
  */
 public class ImageItemRowView extends LinearLayout {
-    private ImageItem mImageItem;
+    // Generated from view_image_item_row.xml
+    private ViewImageItemRowBinding mBinding;
+
+    // When this class is compiled, the BindingAdapter will be available and executed when used.
+    @BindingAdapter({"app:imageUrl"})
+    public static void loadImage(ImageButton view, String url) {
+        Picasso.with(view.getContext()).load(url).into(view);
+    }
 
     public ImageItemRowView(Context ctx) {
         super(ctx);
@@ -23,29 +32,30 @@ public class ImageItemRowView extends LinearLayout {
     public ImageItemRowView(Context ctx, ImageItem imageItem) {
         this(ctx);
 
-        LayoutInflater.from(ctx).inflate(R.layout.view_image_item_row, this, true);
+        mBinding = DataBindingUtil.inflate(LayoutInflater.from(ctx), R.layout.view_image_item_row, this, true);
         setImageItem(imageItem);
     }
 
     private void loadView() {
-        // title
-        TextView titleTextView = (TextView) findViewById(R.id.title);
-        titleTextView.setText(getImageItem().getTitle());
-        titleTextView.setSelected(true);
-
-        // image
-        ImageView imageView = (ImageView) findViewById(R.id.image);
-        Picasso picasso = Picasso.with(getContext());
-        picasso.load(getImageItem().getImageUrlString())
-                .into(imageView);
+        mBinding.title.setSelected(true);
     }
 
     public void setImageItem(ImageItem imageItem) {
-        mImageItem = imageItem;
+        mBinding.setItem(new RowData(imageItem));
+        // update views from the binding info
+        mBinding.executePendingBindings();
+
+        // perform custom updating
         loadView();
     }
 
-    public ImageItem getImageItem() {
-        return mImageItem;
+    public static class RowData {
+        public String title;
+        public String imageUrl;
+
+        public RowData(ImageItem item) {
+            title = item.getTitle();
+            imageUrl = item.getImageUrlString();
+        }
     }
 }
